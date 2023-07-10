666d4713728ec892e76ddba4dfea2638f5e3a7c8
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2023-07-10 15:03:32 +0330

    Initial Commit

---
+++ coreData-ex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreData-ex
+//
+//  Created by Ali Esmaeili on 7/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
